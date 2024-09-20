e0e192832e5474cb9a34dd48be171092034c1df5
rozam007 <rozam0040@gmail.com>
2024-09-21 00:56:47 +0500

    file for browser body

---
+++ webapp/channels/src/components/browser_view/browser_body/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Browser_Body = () => {
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+           
+        </div>
+    );
+};
+
+export default Browser_Body;
