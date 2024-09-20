f1f8c087b7687c9e1d548303d36835fc5187bfaf
rozam007 <rozam0040@gmail.com>
2024-09-21 00:56:25 +0500

    main file for browser UI file, containing all three sub components, header, search bar and body

---
+++ webapp/channels/src/components/browser_view/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Browser_Header from './browser_header'
+import Browser_Search_Section from './browser_search_section'
+import Browser_Body from './browser_body'
+
+const Browser_View = () => {
+  return (
+    <>
+    <Browser_Header/>
+    <Browser_Search_Section/>
+    <Browser_Body/>
+    </>
+  )
+}
+
+export default Browser_View
